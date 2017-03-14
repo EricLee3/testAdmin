65a0610387157808b5fbd6ace18b2e708c0f2123
EricLee3 <in.lee@isecommerce.co.kr>
2017-03-14 17:51:01 +0900

    added admin bootstrap template

--- src/main/java/com/testAdmin/infrastructure/controller/HelloRestController.java
+++ src/main/java/com/testAdmin/infrastructure/controller/HelloRestController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +17,10 @@ public class HelloRestController {
 	private HelloDao helloDao;
 	
 	@RequestMapping("/add")
-	public Hello add(Hello hello)  {
+	public String add(Hello hello, Model model)  {
 		Hello helloData = helloDao.save(hello);
-		return helloData;
+		model.addAttribute("helloData", helloData.getId());
+		return "add";
 	}
 	
 	@RequestMapping("/list")
@@ -29,15 +29,16 @@ public class HelloRestController {
 		return helloList;
 	}
 	
-	/*
+	
 	@RequestMapping("/")
-	public String index()  {
-		return "Hello World!!!!!!";
+	public String index1()  {
+		return "../index";
+		//return "Hello World!!!!!!";
 	}
-	*/
+	
 	
 	@RequestMapping("/hello")
-	public String index()  {
+	public String index(Model model)  {
 		//model.addAttribute("name", "SpringBoot test Administrator");
 		return "hello";
 	}
